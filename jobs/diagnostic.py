9520be919402d74eb5e1e95184561df6038dffdc
sterlingbaldwin <sterling16@mac.com>
2016-11-17 11:30:55 -0800

    Working on getting the diagnostic job working

--- jobs/diagnostic.py
+++ jobs/diagnostic.py
@@ -1,6 +1,9 @@
 from subprocess import Popen, PIPE
+from time import sleep
 from util import print_debug
 from util import print_message
+from pprint import pformat
+
 
 
 class Diagnostic(object):
@@ -10,25 +13,49 @@ class Diagnostic(object):
     def __init__(self, config=None):
         self.config = {}
         self.status = self.validate(config)
+        self.inputs = [
+            'model_path',
+            'obs_path',
+            'output_path',
+            'package',
+            'set',
+            'archive'
+        ]
+
+    def __str__(self):
+        return pformat({
+            'config': self.config,
+            'status': self.status
+        }, indent=4)
 
     def execute(self):
         """
-            Executes the diagnostic job
+            Executes the diagnostic job.
+            If archive is set to True, will create a tarbal of the output directory
         """
-        print_message('Im totally executing', 'ok')
+        cmd = ['metadiags']
+        for i in self.config:
+            cmd.append(i)
+            cmd.append(self.config[i])
+        try:
+            proc = Popen(cmd, stdout=PIPE)
+            done = 2
+            while done != 0:
+                done = proc.poll()
+                line = proc.stdout.readline()
+                if done < 0:
+                    break
+                sleep(1)
+        except Exception as e:
+            print_debug(e)
+            print_message('Error running diagnostic')
 
     def validate(self, config=None):
         """
             Validates the config options
             Valid options are: model_path, obs_path, output_path, package, sets
         """
-        valid_arguments = [
-            'model_path',
-            'obs_path',
-            'output_path',
-            'package',
-            'sets'
-        ]
+
         for i in config:
             if i not in valid_arguments:
                 print_message('Unexpected argument: {}, {}'.format(i, config[i]))
@@ -48,7 +75,11 @@ class Diagnostic(object):
                     default = '.'
                 elif i == 'package':
                     default = 'amwg'
-                elif i == 'sets':
+                elif i == 'set':
                     default = '5'
-                print_message('{} not found in config, using {}'.format(i, default))
+                elif i == 'archive':
+                    default = 'False'
+                self.config[i] = default
+                print_message('{} not found in config, using {}'.format(i, default), 'ok')
+        return 'valid'
 
--- workflow.py
+++ workflow.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
+# pylint: disable=C0103
+# pylint: disable=C0111
 import argparse
+from jobs.Diagnostic import Diagnostic
+from util import print_debug
+from util import print_message
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model-path', help='A path to the model output')
 parser.add_argument('-c', '--climos', help='Compute climotologies from model output')
-parser.add_argument('-d', '--diagnostic', help='Run ACME diagnostics')
+parser.add_argument('-d', '--diagnostic', help='Run ACME diagnostics', action='store_true')
 parser.add_argument('-t', '--transfer', help='Transfer the diagnostics')
 parser.add_argument('-p', '--publish', help='Publish the output')
 parser.add_argument('-f', '--file', help='config file')
 
 if __name__ == "__main__":
-    arguments = parser.parse_args()
-    print arguments
+    args = parser.parse_args()
+    print args
+    if args.diagnostic:
+        config = {
+            'model_path': args.model_path,
+            'obs_path': '/test',
+            'not_valid': 'sudo rm -rf /'
+        }
+        d = Diagnostic(config)
+        if d.status == 'valid':
+            d.execute()
+        else:
+            print_message('Invalid diagnostic configuration: {}'.format(d))
+
