6e165478bc24303d7fe40d7d6adf47e9685d5122
sterlingbaldwin <sterling16@mac.com>
2016-11-18 15:15:47 -0800

    Updates the pipeline to support Transfer jobs

--- jobs/Pipeline.py
+++ jobs/Pipeline.py
@@ -1,37 +1,66 @@
+# pylint: disable=C0103
+# pylint: disable=C0111
+# pylint: disable=C0301
+import json
 from util import print_debug
 from util import print_message
 
 
 class Pipeline(object):
-
+    """
+        A job pipeline. Stores, validates, and executes ACME workflow jobs.
+    """
     def __init__(self):
         self.pipe = []
         self.status = 'empty'
         self.outputs = []
+        self.conf_path = ''
 
-    def validate(self):
-        for j in self.pipe:
-            try:
-                if j.validate():
-                    continue
-                else:
-                    self.status = 'invalid'
-                    return
-            except Exception as e:
-                print_debug(e)
-                print_message('Error validating job ' + str(j))
-        self.status = 'valid'
+    def set_conf_path(self, path):
+        self.conf_path = path
 
     def append(self, job):
+        """
+            Adds a job to the pipeline
+        """
         self.pipe.append(job)
+        self.status = 'unvalidated'
+
+    def validate(self):
+        """
+            Validates the pipeline. Sorts the jobs, makes sure
+            the outputs map into the inputs for the next in line.
+            On error returns -1, returns 0 otherwise.
+        """
+        valid = True
+        for job in self.pipe:
+            if job.validate() == -1:
+                valid = False
+        if valid:
+            self.status = 'valid'
+            return 0
+        else:
+            return -1
 
     def execute(self):
+        """
+            Executes all jobs in the pipeline,
+            feeding the outputs from job i into the inputs of job i + 1
+        """
         if self.status != 'valid':
             print_message('Unable to execute in an invalid state')
             return
         for i, j in enumerate(self.pipe):
             outputs = j.execute()
             if i != len(self.pipe) - 1:
-                self.pipe[i + 1].inputs.append(outputs)
+                # self.pipe[i + 1].inputs.update(outputs)
+                if j.type == 'diagnostic' and self.pipe[i + 1].type == 'transfer':
+                    self.pipe[i + 1].config.get('source_endpoint')['path'] = j.outputs.get('output_path')
+                elif j.type == 'transfer' and self.pipe[i + 1].type == 'diagnostic':
+                    self.pipe[i + 1].config['--model'] = j.outputs.get('destination_endpoint').get('path')
+                print self.pipe[i + 1]
+                self.pipe[i + 1].save(self.conf_path)
             self.outputs.append(outputs)
+        with open('workflow_output.json', 'w') as outfile:
+            json.dump(self.outputs, outfile, indent=4)
     
