3ab23999b0caa4162a69d598f91c83fcea1e54a9
sterlingbaldwin <sterling16@mac.com>
2016-11-18 15:16:41 -0800

    Updates the workflow to support transfer jobs

--- workflow.py
+++ workflow.py
@@ -5,6 +5,7 @@
 import argparse
 import json
 from jobs.Diagnostic import Diagnostic
+from jobs.Transfer import Transfer
 from jobs.Pipeline import Pipeline
 from util import print_debug
 from util import print_message
@@ -21,14 +22,28 @@ parser.add_argument('-f', '--file', help='config file')
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    pipe = Pipeline()
     if args.file:
         with open(args.file, 'r') as conf:
             config = json.load(conf)
+            pipe.set_conf_path(args.file)
     if args.diagnostic:
         diag_conf = config.get('diagnostic')
         d = Diagnostic(diag_conf)
         if d.status == 'valid':
-            d.execute()
+            pipe.append(d)
         else:
             print_message('Invalid diagnostic configuration: {}'.format(d))
+    if args.transfer:
+        transfer_config = config.get('transfer')
+        t = Transfer(transfer_config)
+        print t
+        if t.status == 'valid':
+            pipe.append(t)
+        else:
+            print_message('Invalid transfer configuration: {}'.format(t))
+    if pipe.validate() == -1:
+        print_message('Invalid Pipeline')
+    else:
+        pipe.execute()
 
