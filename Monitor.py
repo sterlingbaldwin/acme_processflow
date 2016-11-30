08c668108f97f20fbd52c9f9c5939d68191516cc
sterlingbaldwin <sterling16@mac.com>
2016-11-30 10:12:30 -0800

    The monitor class is now working

--- Monitor.py
+++ Monitor.py
@@ -1,4 +1,6 @@
 from time import sleep
+from util import print_debug
+from util import print_message
 import paramiko
 
 
@@ -7,18 +9,102 @@ class Monitor(object):
         A class to monitor a remote directory, and pull down any files matching the given regex
     """
     def __init__(self, config=None):
+        """
+            Initializes the monitoring system,
+            inputs: remote_host, remote_dir, username, password (optional), keyfile (optional)
+        """
         if not config:
             print "No configuration for monitoring system"
-            return -1
+            return
+        print config
         self.remote_host = config.get('remote_host')
-        if not self.host:
+        if not self.remote_host:
             print "No remote host specified"
-            return -1
+            return
         self.remote_dir = config.get('remote_dir')
         if not self.remote_dir:
             print "No remote directory specified"
-            return -1
+            return
         self.username = config.get('username')
         if not self.username:
             print "No username given"
-            return -1
+            return
+        self.pattern = config.get('pattern')
+        if not self.pattern:
+            print "No search pattern given"
+            return
+        self.password = config.get('password')
+        self.keyfile = config.get('keyfile')
+        self.client = None
+        self.known_files = []
+        self.new_files = []
+
+    def connect(self):
+        """
+            Connects to the remote host over ssh
+        """
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if self.keyfile:
+            try:
+                self.client.connect(
+                    port=22,
+                    hostname=self.remote_host,
+                    username=self.username,
+                    key_filename=self.keyfile
+                )
+            except Exception as e:
+                print "Unable to connect to remote host with given private key"
+                print_debug(e)
+                return -1
+        else:
+            try:
+                self.client.connect(
+                    port=22,
+                    hostname=self.remote_host,
+                    username=self.username,
+                    password=self.password
+                )
+            except Exception as e:
+                print "Unable to connect to host with given username/password"
+                print_debug(e)
+                return -1
+        return 0
+
+    def set_known_files(self, files):
+        """
+            Sets the list of known files.
+            inputs: files, a list of filenames
+        """
+        self.known_files = files
+
+    def get_known_files(self):
+        """
+            Returns the list of known files
+        """
+        return self.known_files
+
+    def check(self):
+        """
+            Checks to remote_dir for any files that arent in the known files list
+        """
+        cmd = 'ls {path} | grep {pattern}'.format(
+            path=self.remote_dir,
+            pattern=self.pattern
+        )
+        stdin, stdout, stderr = self.client.exec_command(cmd)
+        files = stdout.read()
+        files.strip()
+        files = files.split()
+        self.new_files = []
+        for f in files:
+            if f not in self.known_files:
+                self.known_files.append(f)
+                self.new_files.append(f)
+
+    def get_new_files(self):
+        """
+            Returns a list of only the new files that have been added since the last check
+        """
+        return self.new_files
