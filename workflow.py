5ce38bf594566fa0bc37b7f3d356db94af12603f
sterlingbaldwin <sterling16@mac.com>
2016-12-07 10:10:30 -0800

    Updates input checker to ask nicely for users password

--- workflow.py
+++ workflow.py
@@ -4,46 +4,51 @@
 # pylint: disable=C0301
 import argparse
 import json
+import sys
+from getpass import getpass
 from jobs.Diagnostic import Diagnostic
 from jobs.Transfer import Transfer
-from jobs.Pipeline import Pipeline
+from Monitor import Monitor
 from util import print_debug
 from util import print_message
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model', help='Run the ACME model', action='store_true')
-parser.add_argument('-c', '--climos', help='Compute climotologies from model output', action='store_true')
-parser.add_argument('-d', '--diagnostic', help='Run ACME diagnostics', action='store_true')
-parser.add_argument('-t', '--transfer', help='Transfer the diagnostics', action='store_true')
-parser.add_argument('-p', '--publish', help='Publish the output', action='store_true')
-parser.add_argument('-cm', '--cmore', help='Run CMOR on the model output', action='store_true')
-parser.add_argument('-ud', '--upload-diagnostic', help='upload the diagnostic output to the diagnostic viewer', action='store_true')
-parser.add_argument('-f', '--file', help='config file')
+parser.add_argument('-c', '--config', help='Path to configuration file')
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    pipe = Pipeline()
-    if args.file:
-        with open(args.file, 'r') as conf:
-            config = json.load(conf)
-            pipe.set_conf_path(args.file)
-    if args.diagnostic:
-        diag_conf = config.get('diagnostic')
-        d = Diagnostic(diag_conf)
-        if d.status == 'valid':
-            pipe.append(d)
-        else:
-            print_message('Invalid diagnostic configuration: {}'.format(d))
-    if args.transfer:
-        transfer_config = config.get('transfer')
-        t = Transfer(transfer_config)
-        print t
-        if t.status == 'valid':
-            pipe.append(t)
-        else:
-            print_message('Invalid transfer configuration: {}'.format(t))
-    if pipe.validate() == -1:
-        print_message('Invalid Pipeline')
+    if args.config:
+        with open(args.config, 'r') as conf:
+            try:
+                config = json.load(conf)
+            except Exception as e:
+                print_debug(e)
+                print_message('Unable to read config file, is it properly formatted json?')
+                sys.exit()
+        required_fields = [
+            "output_path",
+            "data_cache_path",
+            "compute_host",
+            "compute_user",
+            "compute_password",
+            "processing_host",
+            "processing_user",
+            "processing_password",
+            "globus_user",
+            "gobus_password",
+            "source_endpoint",
+            "destination_endpoint",
+            "source_path",
+        ]
+        for field in required_fields:
+            if field not in config or len(config[field]) == 0:
+                if 'password' in field:
+                    config[field] = getpass("{0} not specified in config, please enter:".format(field))
+                else:
+                    config[field] = raw_input("{0} not specified in config, please enter:".format(field))
     else:
-        pipe.execute()
+       print 'No configuration file given, initiating manual setup'
+    
+
+
 
