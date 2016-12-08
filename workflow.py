0ab13e2fd642f46f1acead2157777f2c747c8222
sterlingbaldwin <sterling16@mac.com>
2016-12-08 15:00:17 -0800

    Updates the transfer to handle it in a sepperate thread

--- workflow.py
+++ workflow.py
@@ -6,6 +6,9 @@ import argparse
 import json
 import sys
 import os
+import re
+import threading
+from math import floor
 from shutil import copy, rmtree
 from getpass import getpass
 from jobs.Diagnostic import Diagnostic
@@ -14,11 +17,17 @@ from Monitor import Monitor
 from util import print_debug
 from util import print_message
 
+global debug
+debug = False
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', help='Path to configuration file')
+parser.add_argument('-d', '--debug', help="Run in debug mode", action='store_true')
 
 def setup(parser):
     args = parser.parse_args()
+    if args.debug:
+        debug = True
     required_fields = [
         "output_path",
         "data_cache_path",
@@ -59,12 +68,33 @@ def setup(parser):
                 config[field] = raw_input('{0}: '.format(field))
     return config
 
+def handle_transfer(transfer_job):
+    if debug:
+        print_message("starting transfer job", 'ok')
+    transfer_job.execute()
+    for file in monitor.get_new_files():
+        copy(
+            src='{tmp}/{file}'.format(tmp=tmpdir, file=file),
+            dst=config.get('data_cache_path') + '/'
+        )
+    rmtree(tmpdir)
+    if debug:
+        print_message("transfer job complete", 'ok')
+
 if __name__ == "__main__":
 
+    # Initialize the job list
+    job_list = {}
+    job_counter = 0
+
+    # Read in parameters from config
     config = setup(parser)
     if config == -1:
         print "Error in setup, exiting"
         sys.exit()
+    year_sets = (int(config.get('simulation_end_year')) - int(config.get('simulation_start_year'))) / int(config.get('set_frequency'))
+    job_sets = [{'status': 'no data'} for i in range(year_sets)]
+    # Setup remote monitoring system
     monitor = Monitor({
         'remote_host': config.get('compute_host'),
         'remote_dir': config.get('source_path'),
@@ -79,29 +109,37 @@ if __name__ == "__main__":
         print_message('unable to connect')
     monitor.check()
     print_message('Found new files: {}, setting up transfer'.format(monitor.get_new_files()), 'ok')
-    file_list = ['{path}/{file}'.format(path=config.get('source_path'), file=f)  for f in monitor.get_new_files()]
-    tmpdir = os.getcwd() + '/tmp/'
-    t = Transfer(config={
-        'file_list': file_list,
-        'globus_username': config.get('globus_username'),
-        'globus_password': config.get('globus_password'),
-        'source_username': config.get('compute_username'),
-        'source_password': config.get('compute_password'),
-        'destination_username': config.get('processing_username'),
-        'destination_password': config.get('processing_password'),
-        'source_endpoint': config.get('source_endpoint'),
-        'destination_endpoint': config.get('destination_endpoint'),
-        'source_path': config.get('source_path'),
-        'destination_path': tmpdir,
-        'recursive': 'False'
-    })
-    t.execute()
-    for file in monitor.get_new_files():
-        copy(
-            src='{tmp}/{file}'.format(tmp=tmpdir, file=file),
-            dst=config.get('data_cache_path') + '/'
-        )
-    rmtree(tmpdir)
+    if monitor.get_new_files():
+        if debug:
+            print "New remote files found {}, starting transfer".format(monitor.get_new_files)
+
+        # find which year set the data belongs to
+        for f in monitor.get_new_files():
+            index = re.search("[0-9][0-9][0-9][0-9]", f).start()
+            year_set = int(floor(int(f[index: index + 4]) / config.get('set_frequency')))
+            year_sets[year_set].status = 'data in transit'
+
+        file_list = ['{path}/{file}'.format(path=config.get('source_path'), file=f)  for f in monitor.get_new_files()]
+        tmpdir = os.getcwd() + '/tmp/'
+        t = Transfer({
+            'file_list': file_list,
+            'globus_username': config.get('globus_username'),
+            'globus_password': config.get('globus_password'),
+            'source_username': config.get('compute_username'),
+            'source_password': config.get('compute_password'),
+            'destination_username': config.get('processing_username'),
+            'destination_password': config.get('processing_password'),
+            'source_endpoint': config.get('source_endpoint'),
+            'destination_endpoint': config.get('destination_endpoint'),
+            'source_path': config.get('source_path'),
+            'destination_path': tmpdir,
+            'recursive': 'False'
+        })
+        job_counter += 1
+        job_list[job_counter] = t
+        thread = threading.Thread(target=handle_transfer, args=t)
+        thread.start()
+
 
 
 
