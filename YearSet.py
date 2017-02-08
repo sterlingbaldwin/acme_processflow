3950060c9124e855417d073a8dd98510b5f5e3d6
sterlingbaldwin <sterling16@mac.com>
2017-02-07 16:08:40 -0800

    Updates the YearSet to calculate its frequency

--- YearSet.py
+++ YearSet.py
@@ -15,12 +15,17 @@ class YearSet(object):
     def __init__(self, set_number, start_year, end_year):
         """
         Initialize member variables
+
+        set_number, the identifier for this set
+        start_year, the simulation year this set starts
+        end_year, the simulation yaer this set ends
         """
         self.status = SetStatus.NO_DATA
         self.jobs = []
         self.set_number = set_number
         self.set_start_year = start_year
         self.set_end_year = end_year
+        self.freq = self.set_end_year - self.set_start_year + 1
 
     def add_job(self, job):
         self.jobs.append(job)
