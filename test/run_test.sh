dc670d9a7e70f6f0106c09b98eb47f808620d96b
Sterling Baldwin <baldwin32@llnl.gov>
2017-09-01 12:04:57 -0700

    Updates the test script

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
 echo "activating environment"
 source activate /p/cscratch/acme/bin/acme
 
-echo "removing cached ATM files"
-rm /p/cscratch/acme/baldwin32/ci-bot-test/input/ATM/case_scripts.cam.h0.0005-12.nc
+echo "removing cached ATM file to trigger transfer"
+if [ -f /p/cscratch/acme/acme_test_user/ci-bot-test/input/ATM/case_scripts.cam.h0.0005-12.nc ]; then
+    rm /p/cscratch/acme/acme_test_user/ci-bot-test/input/ATM/case_scripts.cam.h0.0005-12.nc
+fi
 
 echo "starting tests"
-nohup python /export/baldwin32/projects/acme_workflow/workflow.py -c /export/baldwin32/projects/acme_workflow/test_run.cfg --no-ui &
+python /home/acme_test_user/github/acme_workflow/workflow.py -c /home/acme_test_user/github/acme_workflow/tests/test_run.cfg --no-ui &
