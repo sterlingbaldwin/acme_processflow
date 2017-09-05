713b2cc068e2c5898a909f7867e4083027f563ec
Sterling Baldwin <baldwin32@llnl.gov>
2017-09-05 14:02:00 -0700

    Updating test scripts

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,10 +1,19 @@
 echo "activating environment"
-source activate /p/cscratch/acme/bin/acme
+source activate workflow
+
+BASE_DIR=/p/cscratch/acme/baldwin32/ci-bot-test
+OUTPUT_DIR=$BASE_DIR/output
+WORKFLOW_DIR=/export/baldwin32/github/acme_workflow
 
 echo "removing cached ATM file to trigger transfer"
-if [ -f /p/cscratch/acme/acme_test_user/ci-bot-test/input/ATM/case_scripts.cam.h0.0005-12.nc ]; then
-    rm /p/cscratch/acme/acme_test_user/ci-bot-test/input/ATM/case_scripts.cam.h0.0005-12.nc
+if [ -f $BASE_DIR/input/ATM/case_scripts.cam.h0.0005-12.nc ]; then
+    rm $BASE_DIR/input/ATM/case_scripts.cam.h0.0005-12.nc
+fi
+
+echo "removing cached output"
+if [ -d $OUTPUT_DIR ]; then
+    rm -rf $OUTPUT_DIR
 fi
 
 echo "starting tests"
-python /home/acme_test_user/github/acme_workflow/workflow.py -c /home/acme_test_user/github/acme_workflow/tests/test_run.cfg --no-ui &
+python $WORKFLOW_DIR/workflow.py -c $WORKFLOW_DIR/tests/test_run.cfg --no-ui &
