977b7064a28b0622e98f72aabae76ae58619520f
Sterling Baldwin <baldwin32@llnl.gov>
2017-09-07 13:57:48 -0700

    Getting there

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import sys
 from setuptools import find_packages, setup
 
+data_files = [('resources', [
+                    'acme_diags_tempalte.py',
+                    'amwg_template.csh',
+                    'config_template.json',
+                    'run_AIMS_template.csh'])]
+
 setup(
     name="acme_workflow",
     version="0.3.0",
@@ -8,5 +14,6 @@ setup(
     author_email="baldwin32@llnl.gov",
     description="ACME Automated Workflow.",
     scripts=["workflow.py"],
-    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"])
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    data_files=data_files
 )
