ede19c1ea1753d346fa16b8e2adf4701f52bc7f7
Sterling Baldwin <baldwin32@llnl.gov>
2018-01-03 11:22:58 -0800

    Suppresses excess logging messages from globus_cli

--- lib/initialize.py
+++ lib/initialize.py
@@ -204,7 +204,8 @@ Please add a space and run again.'''.format(num=line_index)
         filename=log_path,
         filemode='w',
         level=logging.INFO)
-    logging.getLogger('globus_sdk').setLevel(logging.WARNING)
+    logging.getLogger('globus_sdk').setLevel(logging.ERROR)
+    logging.getLogger('globus_cli').setLevel(logging.ERROR)
 
     # Make sure the set_frequency is a list of ints
     set_frequency = config['global']['set_frequency']
