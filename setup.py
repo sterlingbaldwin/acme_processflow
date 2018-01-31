6918820cc404d9e930a4716f5e8ea01ab4801d10
Sterling Baldwin <baldwin32@llnl.gov>
2018-01-31 11:01:01 -0800

    New nightly build

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ data_files = [(sys.prefix + '/share/processflow/resources',
 
 setup(
     name="acme_processflow",
-    version="0.4.64",
+    version="0.4.65",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
     description="ACME Automated Processflow for handling post processing jobs for raw model data",
