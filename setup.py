e42b19b4f9837696c568102d778e7357d42346fa
Sterling Baldwin <baldwin32@llnl.gov>
2018-05-15 16:05:10 -0700

    New master build 1.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ data_files = [(sys.prefix + '/share/processflow/resources',
 
 setup(
     name="acme_processflow",
-    version="1.1.2",
+    version="1.1.3",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
     description="ACME Automated Processflow for handling post processing jobs for raw model data",
