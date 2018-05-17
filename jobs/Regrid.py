b3ac26080330b7ae234c2687b416e7e5fa2cc2e1
Sterling Baldwin <baldwin32@llnl.gov>
2018-05-16 22:08:50 -0700

    Adds checks for input files, as well as source grid and destination grid paths

--- jobs/Regrid.py
+++ jobs/Regrid.py
@@ -51,7 +51,8 @@ class Regrid(object):
             'output_path': '',
             'regrid_map_path': '',
             'year_set': '',
-            'run_scripts_path': ''
+            'run_scripts_path': '',
+            'file_type': ''
         }
         self.slurm_args = {
             'num_cores': '-n 16',  # 16 cores
@@ -73,8 +74,10 @@ class Regrid(object):
         if config.get('ui') is None:
             config['ui'] = False
         
-        if config['data_type'] not in ['mpas', 'clm', 'cam']:
+        if config['data_type'] not in ['ocn', 'lnd', 'atm']:
             self.status = JobStatus.INVALID
+            msg = 'regrid-{}-{}: invalid data_type requested'.format(self.start_year, self.end_year)
+            logging.error(msg)
             return
 
         # Assuming monthly history files
@@ -91,8 +94,8 @@ class Regrid(object):
                     break
 
         if len(expected_files) > 0:
-            msg = 'regrid-{start:04d}-{end:904d}: missing input files {}'.format(
-                start=self.start_year, end=seld.end_year, expected_files)
+            msg = 'regrid-{start:04d}-{end:04d}: missing input files {files}'.format(
+                start=self.start_year, end=seld.end_year, files=expected_files)
             logging.error(msg)
             self.status = JobStatus.INVALID
             return
@@ -102,77 +105,12 @@ class Regrid(object):
             os.makedirs(self.config.get('run_scripts_path'))
         if self.year_set == 0:
             self.status = JobStatus.INVALID
+            return
+        
+        # nothing has marked the status as invalid so we should be good
+        self.status = JobStatus.VALID
         return 0
-
-    def execute(self, dryrun=False):
-        """
-        Calls ncclimo in a subprocess
-        """
-
-        # check if the output already exists and the job actually needs to run
-        if self.postvalidate():
-            self.status = JobStatus.COMPLETED
-            return 0
-
-        self.output_path = self.config['regrid_output_directory']
-
-        cmd = ['ncremap']
-
-        if self.config['data_type'] == 'lnd':
-            cmd += ['-P', 'clm']
-        elif self.config['data_type'] == 'ocn':
-            cmd += ['-P', 'mpas']
-        elif self.config['data_type'] == 'atm':
-            cmd += ['-P', 'alm']
-        else:
-            msg = 'Unsupported regrid type'
-            logging.error(msg)
-            self.status = FAILED
-            return 0
-
-        cmd += ['-i', self.config['input_path'],
-                '-o', self.config['output_path'],
-                '-m', self.config['regrid_map_path']]
-        slurm_command = ' '.join(cmd)
-
-        # Submitting the job to SLURM
-        expected_name = '{type}_{start:04d}_{end:04d}'.format(
-            start=self.config.get('start_year'),
-            end=self.config.get('end_year'),
-            type=self.type)
-        run_script = os.path.join(self.config.get(
-            'run_scripts_path'), expected_name)
-        if os.path.exists(run_script):
-            os.remove(run_script)
-
-        self.slurm_args['output_file'] = '-o {output_file}'.format(
-            output_file=run_script + '.out')
-        slurm_prefix = '\n'.join(['#SBATCH ' + self.slurm_args[s]
-                                  for s in self.slurm_args]) + '\n'
-
-        with open(run_script, 'w') as batchfile:
-            batchfile.write('#!/bin/bash\n')
-            batchfile.write(slurm_prefix)
-            batchfile.write(slurm_command)
-
-        if dryrun:
-            self.status = JobStatus.COMPLETED
-            return 0
-
-        slurm = Slurm()
-        msg = 'Submitting to queue {type}: {start:04d}-{end:04d}'.format(
-            type=self.type,
-            start=self.start_year,
-            end=self.end_year)
-        print_line(
-            ui=self.config.get('ui', False),
-            line=msg,
-            event_list=self.event_list,
-            current_state=True)
-        self.job_id = slurm.batch(run_script, '--oversubscribe')
-
-        return self.job_id
-
+    
     def postvalidate(self):
         """
         Post execution validation, also run before execution to determine if the output already extists
@@ -207,13 +145,121 @@ class Regrid(object):
                     break
 
         if len(expected_files) > 0:
-            msg = 'regrid-{start:04d}-{end:904d}: missing output files {}'.format(
-                start=self.start_year, end=seld.end_year, expected_files)
+            msg = 'regrid-{start:04d}-{end:04d}: missing output files {files}'.format(
+                start=self.start_year, end=self.end_year, files=expected_files)
             logging.error(msg)
-            self.status = JobStatus.FAILED
             return False
 
         return True
+
+    def get_input_file_list(self):
+        """
+        Gets the list of required input files, returns False if all needed files arent found
+        """
+        # first build the list of expected files
+        input_files = list()
+        for year in range(self.start_year, self.end_year + 1):
+            for month in range(1, 13):
+                nfilestr = ''
+                if self.data_type in ['atm', 'lnd']:
+                    nfilestr += self.config['caseId'] + '.'
+                nfilestr += '{file_type}.{year:04d}-{month:02d}'.format(
+                    file_type=self.config['file_type'],
+                    year=year,
+                    month=month)
+                if self.data_type == 'ocn':
+                    nfilestr += '-01'
+                nfilestr += '.nc'
+                input_files.append(
+                    os.path.join(
+                        self.config['input_path'],
+                        nfilestr))
+
+        # second check each file exists
+        for infile in input_files:
+            if not os.path.exists(infile):
+                return False
+        return input_files
+
+
+    def execute(self, dryrun=False):
+        """
+        Calls ncremap in a subprocess
+
+        Returns:
+            0 if precomputed
+            -1 if waiting on additional data
+
+        """
+
+        # check if the output already exists and the job actually needs to run
+        if self.postvalidate():
+            self.status = JobStatus.COMPLETED
+            return 0
+        
+        input_files = self.get_input_file_list()
+        if not input_files:
+            return -1
+        self.output_path = self.config['output_path']
+
+        cmd = ['ncremap']
+
+        if self.config['data_type'] == 'lnd':
+            cmd += ['-P', 'clm']
+        elif self.config['data_type'] == 'ocn':
+            cmd += ['-P', 'mpas']
+        elif self.config['data_type'] == 'atm':
+            cmd += ['-P', 'alm']
+        else:
+            msg = 'Unsupported regrid type'
+            logging.error(msg)
+            self.status = FAILED
+            return 0
+
+        cmd += ['-O', self.config['output_path'],
+                '-s', self.config['source_grid_path'],
+                '-d', self.config['destination_grid_path'],
+                ' '.join(input_files)]
+        slurm_command = ' '.join(cmd)
+
+        # Submitting the job to SLURM
+        expected_name = '{type}_{data_type}_{start:04d}_{end:04d}'.format(
+            start=self.config.get('start_year'),
+            end=self.config.get('end_year'),
+            type=self.type,
+            data_type=self.config['data_type'])
+        run_script = os.path.join(self.config.get(
+            'run_scripts_path'), expected_name)
+        if os.path.exists(run_script):
+            os.remove(run_script)
+
+        self.slurm_args['output_file'] = '-o {output_file}'.format(
+            output_file=run_script + '.out')
+        slurm_prefix = '\n'.join(['#SBATCH ' + self.slurm_args[s]
+                                  for s in self.slurm_args]) + '\n'
+
+        with open(run_script, 'w') as batchfile:
+            batchfile.write('#!/bin/bash\n')
+            batchfile.write(slurm_prefix)
+            batchfile.write(slurm_command)
+
+        if dryrun:
+            self.status = JobStatus.COMPLETED
+            return 0
+
+        slurm = Slurm()
+        msg = 'Submitting to queue {type}: {start:04d}-{end:04d}'.format(
+            type=self.type,
+            start=self.start_year,
+            end=self.end_year)
+        print_line(
+            ui=self.config.get('ui', False),
+            line=msg,
+            event_list=self.event_list,
+            current_state=True)
+        self.job_id = slurm.batch(run_script, '--oversubscribe')
+
+        return self.job_id
 
     def __str__(self):
         return json.dumps({
