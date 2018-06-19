21853069a3dcca8d3b8b1414f6b100442ab1d0d5
Sterling Baldwin <baldwin32@llnl.gov>
2018-06-19 13:30:35 -0700

    Cleans up and better organizes the initialization step

--- lib/initialize.py
+++ lib/initialize.py
@@ -35,10 +35,6 @@ def parse_args(argv=None, print_help=None):
         '-l', '--log',
         help='Path to logging output file.')
     parser.add_argument(
-        '-n', '--no-host',
-        help='Don\'t move output plots into the web host directory.',
-        action='store_true')
-    parser.add_argument(
         '-s', '--scripts',
         help='Copy the case_scripts directory from the remote machine.',
         action='store_true')
@@ -76,25 +72,23 @@ def initialize(argv, **kwargs):
     Parse the commandline arguments, and setup the master config dict
 
     Parameters:
-        parser (argparse.ArgumentParser): The parser object
+        argv (list): a list of arguments
         event_list (EventList): The main list of events
-        thread_list (list): the main list of all running threads
         mutex (threading.Lock): A mutex to handle db access
         kill_event (threading.Event): An event used to kill all running threads
-        version (str): the current version number for processflow
-        branch (str): the branch this version was built from
+        __version__ (str): the current version number for processflow
+        __branch__ (str): the branch this version was built from
     """
     # Setup the parser
-    args = parse_args(argv=argv)
-    if args.version:
+    pargs = parse_args(argv=argv)
+    if pargs.version:
         msg = 'Processflow version {}'.format(kwargs['version'])
         print msg
-        sys.exit()
-    if not args.config:
+        sys.exit(0)
+    if not pargs.config:
         parse_args(print_help=True)
         return False, False, False
     event_list = kwargs['event_list']
-    thread_list = kwargs['thread_list']
     mutex = kwargs['mutex']
     event = kwargs['kill_event']
     print_line(
@@ -109,12 +103,12 @@ def initialize(argv, **kwargs):
         except:
             os.remove(globus_config)
 
-    if not os.path.exists(args.config):
-        print "Invalid config, {} does not exist".format(args.config)
+    if not os.path.exists(pargs.config):
+        print "Invalid config, {} does not exist".format(pargs.config)
         return False, False, False
 
     # Check that there are no white space errors in the config file
-    line_index = check_config_white_space(args.config)
+    line_index = check_config_white_space(pargs.config)
     if line_index != 0:
         print '''
 ERROR: line {num} does not have a space after the \'=\', white space is required.
@@ -123,69 +117,41 @@ Please add a space and run again.'''.format(num=line_index)
 
     # read the config file and setup the config dict
     try:
-        config = ConfigObj(args.config)
+        config = ConfigObj(pargs.config)
     except Exception as e:
         print_debug(e)
-        print "Error parsing config file {}".format(args.config)
+        print "Error parsing config file {}".format(pargs.config)
         parse_args(print_help=True)
         return False, False, False
-
+    
     # run validator for config file
-    if config.get('global'):
-        if args.resource_path:
-            config['global']['resource_path'] = args.resource_path
-        else:
-            config['global']['resource_path'] = os.path.join(
-                sys.prefix,
-                'share',
-                'processflow',
-                'resources')
-    else:
-        return False, False, False
-
-    # Setup boolean config flags
-    config['global']['host'] = False if args.no_host else True
-    config['global']['print_file_list'] = True if args.file_list else False
-    config['global']['scripts'] = True if args.scripts else False
-    config['global']['always_copy'] = True if args.always_copy else False
-    config['global']['dryrun'] = True if args.dryrun else False
-    config['global']['debug'] = True if args.debug else False
-    config['global']['max_jobs'] = args.max_jobs if args.max_jobs else False
-
-    if args.max_jobs:
-        print_line(
-            line="running with maximum {} jobs".format(args.max_jobs),
-            event_list=event_list)
-
     messages = verify_config(config)
     if messages:
         for message in messages:
             print_message(message)
         return False, False, False
-    logging.info("Running with config:")
-    msg = json.dumps(config, sort_keys=False, indent=4)
-    logging.info(msg)
-
-    if args.no_host or not config.get('img_hosting'):
-        print_line(
-            line='Not hosting img output',
-            event_list=event_list)
     
-    # setup input and output directories
-    input_path = os.path.join(
-        config['global']['project_path'],
-        'input')
-    if not os.path.exists(input_path):
-        os.makedirs(input_path)
-    output_path = os.path.join(
-        config['global']['project_path'],
-        'output')
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    if pargs.resource_path:
+        config['global']['resource_path'] = pargs.resource_path
+    else:
+        config['global']['resource_path'] = os.path.join(
+            sys.prefix,
+            'share',
+            'processflow',
+            'resources')
 
-    # setup logging
-    if args.log:
-        log_path = args.log
+    # Setup boolean config flags
+    config['global']['host'] = True if config.get('img_hosting') else False
+    config['global']['print_file_list'] = True if pargs.file_list else False
+    config['global']['scripts'] = True if pargs.scripts else False
+    config['global']['always_copy'] = True if pargs.always_copy else False
+    config['global']['dryrun'] = True if pargs.dryrun else False
+    config['global']['debug'] = True if pargs.debug else False
+    config['global']['max_jobs'] = pargs.max_jobs if pargs.max_jobs else False
+
+     # setup logging
+    if pargs.log:
+        log_path = pargs.log
     else:
         log_path = os.path.join(
             config['global']['project_path'],
@@ -198,7 +164,7 @@ Please add a space and run again.'''.format(num=line_index)
         from imp import reload
         reload(logging)
     config['global']['log_path'] = log_path
-    log_level = logging.DEBUG if args.debug else logging.INFO
+    log_level = logging.DEBUG if pargs.debug else logging.INFO
     logging.basicConfig(
         format='%(asctime)s:%(levelname)s: %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -208,13 +174,27 @@ Please add a space and run again.'''.format(num=line_index)
     logging.getLogger('globus_sdk').setLevel(logging.ERROR)
     logging.getLogger('globus_cli').setLevel(logging.ERROR)
 
+    logging.info("Running with config:")
+    msg = json.dumps(config, sort_keys=False, indent=4)
+    logging.info(msg)
+
+    if pargs.max_jobs:
+        print_line(
+            line="running with maximum {} jobs".format(pargs.max_jobs),
+            event_list=event_list)
+
+    if not config['global']['host'] or not config.get('img_hosting'):
+        print_line(
+            line='Not hosting img output',
+            event_list=event_list)
+
     msg = 'processflow version {} branch {}'.format(
         kwargs['version'],
         kwargs['branch'])
     logging.info(msg)
 
     try:
-        setup_directories(args, config)
+        setup_directories(pargs, config)
     except Exception as e:
         print_message('Failed to setup directories')
         print_debug(e)
@@ -225,7 +205,7 @@ Please add a space and run again.'''.format(num=line_index)
         config['global']['project_path'], 
         'input', 'run.cfg')
     try:
-        copy(args.config, input_config_path)
+        copy(pargs.config, input_config_path)
     except:
         pass
 
@@ -298,7 +278,6 @@ Please add a space and run again.'''.format(num=line_index)
     # setup the runmanager
     runmanager = RunManager(
         event_list=event_list,
-        thread_list=thread_list,
         event=event,
         config=config,
         filemanager=filemanager)
@@ -314,10 +293,10 @@ def setup_directories(_args, config):
     Setup the input, output, pp, and diags directories
     """
     # setup output directory
-
     output_path = os.path.join(
         config['global']['project_path'],
         'output')
+    config['global']['output_path'] = output_path
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
@@ -325,6 +304,7 @@ def setup_directories(_args, config):
     input_path = os.path.join(
         config['global']['project_path'],
         'input')
+    config['global']['input_path'] = input_path
     if not os.path.exists(input_path):
         os.makedirs(input_path)
     for sim in config['simulations']:
@@ -357,4 +337,5 @@ def setup_directories(_args, config):
     config['global']['run_scripts_path'] = run_script_path
     if not os.path.exists(run_script_path):
         os.makedirs(run_script_path)
+
 
